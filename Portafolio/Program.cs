586aa91433d71ba44a2b7c8aebb9b02cb7f3f7d2
JONNYJSC <jjonath21@gmail.com>
2023-03-17 21:40:14 -0600

    Inyección de Dependencias con Interfaces

--- Portafolio/Program.cs
+++ Portafolio/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 //Inyeccion de dependencias
-builder.Services.AddTransient<RepositorioProyectos>();
+builder.Services.AddTransient<IRepositorioProyectos, RepositorioProyectos>();
 
 var app = builder.Build();
 
